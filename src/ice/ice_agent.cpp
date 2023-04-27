9575a9ce65787af6a99b9bd7cf43b998d46d78d3
Faker0715 <949506563@qq.com>
2023-04-27 16:06:12 +0800

    [feat] Calculate the status of iceAgent

--- src/ice/ice_agent.cpp
+++ src/ice/ice_agent.cpp
@@ -26,6 +26,9 @@ namespace xrtc {
         }
         auto channel = new IceTransportChannel(el,_allocator,transport_name,component);
         channel->signal_candidate_allocate_done.connect(this,&IceAgent::on_candidate_allocate_done);
+        channel->signal_receiving_state.connect(this,&IceAgent::_on_ice_receiving_state);
+        channel->signal_writable_state.connect(this,&IceAgent::_on_ice_writable_state);
+        channel->signal_ice_state_changed.connect(this,&IceAgent::_on_ice_state_changed);
         _channels.push_back(channel);
 
         return true;
@@ -63,6 +66,58 @@ namespace xrtc {
         if(channel){
             channel->set_remote_ice_params(ice_params);
         }
+    }
+
+    void IceAgent::_on_ice_receiving_state(IceTransportChannel *) {
+        _update_state();
+    }
+
+    void IceAgent::_on_ice_writable_state(IceTransportChannel *) {
+        _update_state();
+    }
+
+    void IceAgent::_on_ice_state_changed(IceTransportChannel *) {
+        _update_state();
+    }
+
+    void IceAgent::_update_state() {
+        IceTransportState ice_state = IceTransportState::k_new;
+        std::map<IceTransportState,int>  ice_state_counts;
+        for(auto channel: _channels){
+            ice_state_counts[channel->state()]++;
+        }
+        int total_ice_failed = ice_state_counts[IceTransportState::k_failed];
+        int total_ice_disconnected = ice_state_counts[IceTransportState::k_disconnected];
+        int total_ice_connected = ice_state_counts[IceTransportState::k_connected];
+        int total_ice_completed = ice_state_counts[IceTransportState::k_completed];
+        int total_ice_new = ice_state_counts[IceTransportState::k_new];
+        int total_ice_checking = ice_state_counts[IceTransportState::k_checking];
+        int total_ice_closed = ice_state_counts[IceTransportState::k_closed];
+        int total_ice = _channels.size();
+        if(total_ice_failed > 0){
+            ice_state = IceTransportState::k_failed;
+        }else if(total_ice_disconnected > 0){
+            ice_state = IceTransportState::k_disconnected;
+        }else if(total_ice_new + total_ice_closed == total_ice){
+            ice_state = IceTransportState::k_new;
+        }else if(total_ice_new + total_ice_checking > 0){
+            ice_state = IceTransportState::k_checking;
+        }else if(total_ice_completed + total_ice_closed == total_ice) {
+            ice_state = IceTransportState::k_completed;
+        }else if(total_ice_connected + total_ice_closed + total_ice_completed == total_ice) {
+            ice_state = IceTransportState::k_connected;
+        }
+
+        if(_ice_state != ice_state){
+            // 为了保证不跳过k_connected状态
+            if(_ice_state == IceTransportState::k_checking && ice_state == IceTransportState::k_completed){
+                signal_ice_state(this,IceTransportState::k_connected);
+            }
+            _ice_state = ice_state;
+            signal_ice_state(this,ice_state);
+        }
+
+
     }
 
 }
--- src/ice/ice_agent.h
+++ src/ice/ice_agent.h
@@ -20,6 +20,7 @@ namespace xrtc {
 
         bool create_channel(EventLoop *el, const std::string &transport_name, IceCandidateComponent component);
 
+        IceTransportState ice_state() { return _ice_state; }
         IceTransportChannel *get_channel(const std::string &transport_name, IceCandidateComponent component);
         void gathering_candidate();
 
@@ -28,6 +29,7 @@ namespace xrtc {
         void set_remote_ice_params(const std::string &transport_name, IceCandidateComponent component,
                             const IceParameters ice_params);
         sigslot::signal4<IceAgent *, const std::string &, IceCandidateComponent, const std::vector<Candidate> &> signal_candidate_allocate_done;
+        sigslot::signal2<IceAgent *, IceTransportState> signal_ice_state;
 
     private:
         std::vector<IceTransportChannel *>::iterator
@@ -35,10 +37,18 @@ namespace xrtc {
 
         void on_candidate_allocate_done(IceTransportChannel *channel, const std::vector<Candidate> &);
 
+        void _on_ice_receiving_state(IceTransportChannel *);
+
+        void _on_ice_writable_state(IceTransportChannel *);
+
+        void _on_ice_state_changed(IceTransportChannel *);
+        void _update_state();
     private:
         PortAllocator *_allocator;
         EventLoop *_el;
         std::vector<IceTransportChannel *> _channels;
+        IceTransportState _ice_state = IceTransportState::k_new;
+
     };
 }
 
