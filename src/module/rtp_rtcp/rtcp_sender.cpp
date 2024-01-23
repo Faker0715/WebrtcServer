cb03dda1bda3c60048032eea7cd0f4d82dfe45e2
Faker0715 <949506563@qq.com>
2024-01-24 00:31:36 +0800

    [feat] Generate packet data based on rtcp packet type

--- src/module/rtp_rtcp/rtcp_sender.cpp
+++ src/module/rtp_rtcp/rtcp_sender.cpp
@@ -7,8 +7,8 @@
 
 namespace xrtc {
 
-    RTCPSender::RTCPSender(const RtpRtcpConfig &config) : clock_(config.clock_){
-
+    RTCPSender::RTCPSender(const RtpRtcpConfig &config) : clock_(config.clock_) {
+        builders_[webrtc::kRtcpSr] = &RTCPSender::BuildRR;
     }
 
     RTCPSender::~RTCPSender() {
@@ -20,11 +20,27 @@ namespace xrtc {
     }
 
     absl::optional<int32_t> RTCPSender::ComputeCompoundRTCPPacket(webrtc::RTCPPacketType packet_type) {
-        if (method_ == webrtc::RtcpMode::kOff){
+        if (method_ == webrtc::RtcpMode::kOff) {
             RTC_LOG(LS_WARNING) << "Can't send rtcp packet when rtcp is off";
         }
-        SetFlag(packet_type,false);
+        SetFlag(packet_type, false);
         PrepareReport();
+        auto it = report_flags_.begin();
+        while(it != report_flags_.end()){
+            if(it->is_valatile){
+                it = report_flags_.erase(it);
+            }else{
+                ++it;
+            }
+            uint32_t rtcp_packet_type = it->type;
+            auto builder_it = builders_.find(rtcp_packet_type);
+            if(builder_it == builders_.end()){
+                RTC_LOG(LS_WARNING) << "Can't find builder for rtcp packet type: " << rtcp_packet_type;
+            }else{
+                BuilderFunc func = builder_it->second;
+                (this->*func)();
+            }
+        }
         return absl::nullopt;
 
     }
@@ -34,27 +50,27 @@ namespace xrtc {
     }
 
     void RTCPSender::SetFlag(uint32_t type, bool is_valatile) {
-        report_flags_.insert(ReportFlag(type,is_valatile));
+        report_flags_.insert(ReportFlag(type, is_valatile));
     }
 
     bool RTCPSender::PrepareReport() {
         bool generate_report = false;
-        if(isFlagPresent(webrtc::kRtcpSr) || IsFlagPresent(webrtc::kRtcpRr) {
+        if (IsFlagPresent(webrtc::kRtcpSr) || IsFlagPresent(webrtc::kRtcpRr)) {
             generate_report = true;
-        }else {
+        } else {
             generate_report = (method_ == webrtc::RtcpMode::kReducedSize && ConsumeFlag(webrtc::kRtcpReport)) ||
                               (method_ == webrtc::RtcpMode::kCompound);
-            if(generate_report){
-                SetFlag(sending_? webrtc::kRtcpSr:webrtc::kRtcpRr, true
+            if (generate_report) {
+                SetFlag(sending_ ? webrtc::kRtcpSr : webrtc::kRtcpRr, true);
             }
 
         }
     }
 
     bool RTCPSender::ConsumeFlag(uint32_t type, bool force) {
-        auto it = report_flags_.find(ReportFlag(type,false));
-        if(it != report_flags_.end()){
-            if(it->is_valatile || force){
+        auto it = report_flags_.find(ReportFlag(type, false));
+        if (it != report_flags_.end()) {
+            if (it->is_valatile || force) {
                 report_flags_.erase(it);
             }
             return true;
@@ -64,8 +80,12 @@ namespace xrtc {
 
 
     bool RTCPSender::IsFlagPresent(uint32_t type) {
-        return report_flags_.find(ReportFlag(type,false)) != report_flags_.end();
+        return report_flags_.find(ReportFlag(type, false)) != report_flags_.end();
+    }
+
+    void RTCPSender::BuildRR() {
+
     }
 }
 
-}
+
--- src/module/rtp_rtcp/rtcp_sender.h
+++ src/module/rtp_rtcp/rtcp_sender.h
@@ -4,46 +4,62 @@
 
 #ifndef XRTCSERVER_RTCP_SENDER_H
 #define XRTCSERVER_RTCP_SENDER_H
+
 #include "module/rtp_rtcp/rtp_rtcp_config.h"
 #include <modules/rtp_rtcp/include/rtp_rtcp_defines.h>
 #include <set>
 
-namespace xrtc{
-    class RTCPSender{
+namespace xrtc {
+    class RTCPSender {
     public:
-        RTCPSender(const RtpRtcpConfig& config);
+        RTCPSender(const RtpRtcpConfig &config);
+
         ~RTCPSender();
+
         void SendRTCP(webrtc::RTCPPacketType packet_type);
+
         void SetRtcpStatus(webrtc::RtcpMode method);
-        void SetSendingStatus(bool sending){
+
+        void SetSendingStatus(bool sending) {
             sending_ = sending;
         };
     private:
         absl::optional<int32_t> ComputeCompoundRTCPPacket(webrtc::RTCPPacketType packet_type);
+
         bool PrepareReport();
+        void BuildRR();
 
+        bool ConsumeFlag(uint32_t type, bool force = false);
 
-        bool ConsumeFlag(uint32_t type,bool force);
         bool IsFlagPresent(uint32_t type);
-        void SetFlag(uint32_t type,bool is_valatile);
+
+        void SetFlag(uint32_t type, bool is_valatile);
+
     private:
-        webrtc::Clock* clock_;
+        webrtc::Clock *clock_;
         webrtc::RtcpMode method_ = webrtc::RtcpMode::kOff;
         bool sending_ = false;
-        struct ReportFlag{
-            ReportFlag(uint32_t type,bool is_valatile):type(type),is_valatile(is_valatile){}
-            bool operator<(const ReportFlag& other) const{
+
+        struct ReportFlag {
+            ReportFlag(uint32_t type, bool is_valatile) : type(type), is_valatile(is_valatile) {}
+
+            bool operator<(const ReportFlag &other) const {
                 return type < other.type;
             }
-            bool operator==(const ReportFlag& other) const{
+
+            bool operator==(const ReportFlag &other) const {
                 return type == other.type;
             }
+
             uint32_t type;
             // 不会消费一次被删除
             bool is_valatile;
         };
+
         // 当前发送的rtcp包的类型
         std::set<ReportFlag> report_flags_;
+        typedef void (RTCPSender::*BuilderFunc)();
+        std::map<uint32_t,BuilderFunc> builders_;
     };
 }
 
