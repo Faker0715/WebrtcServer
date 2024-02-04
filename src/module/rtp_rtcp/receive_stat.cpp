e702f325d7785d28444a75aeb0159587f709727e
Faker0715 <949506563@qq.com>
2024-02-05 00:38:20 +0800

    [feat] Prepare statistical video RTP package receiving information

--- src/module/rtp_rtcp/receive_stat.cpp
+++ src/module/rtp_rtcp/receive_stat.cpp
@@ -17,10 +17,32 @@ namespace xrtc{
 
     void ReceiveStat::OnRtpPacket(const webrtc::RtpPacketReceived &packet) {
 
-        RTC_LOG(LS_INFO) << "ReceiveStat::OnRtpPacket";
+//        RTC_LOG(LS_INFO) << "ReceiveStat::OnRtpPacket";
+        GetOrCreateStat(packet.Ssrc())->UpdateCounters(packet);
     }
 
     std::unique_ptr<ReceiveStat> ReceiveStat::Create(webrtc::Clock *clock) {
         return std::make_unique<ReceiveStat>(clock);
+    }
+
+    StreamStat *ReceiveStat::GetOrCreateStat(uint32_t ssrc) {
+        // 如果这个map没有 就插入一个值
+        std::unique_ptr<StreamStat>& stat = stats_[ssrc];
+        if(nullptr == stat){
+            stat = std::make_unique<StreamStat>(ssrc, clock_);
+        }
+        return stat.get();
+    }
+
+    StreamStat::StreamStat(uint32_t ssrc, webrtc::Clock *clock): ssrc_(ssrc), clock_(clock){
+
+    }
+
+    StreamStat::~StreamStat() {
+
+    }
+
+    void StreamStat::UpdateCounters(const webrtc::RtpPacketReceived &packet) {
+
     }
 }
--- src/module/rtp_rtcp/receive_stat.h
+++ src/module/rtp_rtcp/receive_stat.h
@@ -7,16 +7,31 @@
 
 #include "system_wrappers/include/clock.h"
 #include "modules/rtp_rtcp/source/rtp_packet_received.h"
+#include <rtc_base/containers/flat_map.h>
 
 namespace xrtc{
+    class StreamStat{
+    public:
+        StreamStat(uint32_t ssrc, webrtc::Clock* clock);
+        ~StreamStat();
+        void UpdateCounters(const webrtc::RtpPacketReceived& packet);
+    private:
+        uint32_t ssrc_;
+        webrtc::Clock* clock_;
+
+    };
     class ReceiveStat{
     public:
         ReceiveStat(webrtc::Clock* clock);
         ~ReceiveStat();
         static std::unique_ptr<ReceiveStat> Create(webrtc::Clock* clock);
         void OnRtpPacket(const webrtc::RtpPacketReceived& packet);
+        // 如果有就创建 没有就返回
+        StreamStat* GetOrCreateStat(uint32_t ssrc);
     private:
         webrtc::Clock* clock_;
+        // 需求主要是查询
+        webrtc::flat_map<uint32_t, std::unique_ptr<StreamStat>> stats_;
     };
 }
 
